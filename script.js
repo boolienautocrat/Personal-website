9c9143a06b37902ccee08fb4c9dabdf3c4c14f75
Jose <josetsaji2006@gmail.com>
2023-04-24 15:58:47 +0530

    add progress bar

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var scrolledWin = document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scroll = (scrolledWin/height) * 100;
+
+    document.getElementById("myBar").style.width = scroll + "%";
+}
