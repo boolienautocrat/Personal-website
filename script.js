b717ee5b84febcf2fb381f6ae4d917e9375277d0
Jose <josetsaji2006@gmail.com>
2023-04-25 18:37:58 +0530

    complete desktop & add telegram

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onscroll = function () { myFunction() };
 function myFunction() {
     var scrolledWin = document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scroll = (scrolledWin/height) * 100;
+    var scroll = (scrolledWin / height) * 100;
 
     document.getElementById("myBar").style.width = scroll + "%";
 }
